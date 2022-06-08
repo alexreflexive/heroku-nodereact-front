0b156ba38d69c64fda9b2ce745ee3d9d7dd4777d
Alejandro GABRIEL Y GALAN <alexgyg@msn.com>
2022-06-08 09:55:51 +0200

    front 01

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [documents, setDocuments] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/documents")
+      .then((response) => {
+        console.log(response.data);
+        setDocuments(response.data);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  }, []);
+
   return (
     <div
       style={{ minHeight: "100vh" }}
       className="App container-fluid d-flex justify-content-center align-items-center"
     >
-      <div className="card m-3 w" style={{ width: "200px" }}>
-        <div className="card-header">En-tête</div>
-        <div className="card-body">
-          <div className="card-title">
-            <h4>Titre</h4>
+      {documents.map((document) => {
+        return (
+          <div
+            className="card m-3 w"
+            key={document.id}
+            style={{ width: "200px", minHeight: "300px" }}
+          >
+            <div className="card-header">{document.header}</div>
+            <div className="card-body">
+              <div className="card-title">
+                <h4>{document.title}</h4>
+              </div>
+              <div className="card-text">{document.text}</div>
+            </div>
           </div>
-          <div className="card-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 }
